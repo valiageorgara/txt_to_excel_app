9c28f7c47ce9a5ad8adc4598955949cd20149505
valiageorgara <georgaravalia@gmail.com>
2022-08-26 12:33:37 +0300

    removed white spaces and "", separated switches onto different lines

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import xlsxwriter
 import os
+import re
 
 if __name__ == '__main__':
 
     # Folder Path of txt files
     path = "txt_files"
-
     # Change the directory
     os.chdir(path)
 
@@ -20,16 +20,13 @@ if __name__ == '__main__':
             # store name of file in variable filename
             filename = os.path.splitext(txt_file)[0]
 
-            # Workbook() takes one, non-optional, argument
-            # which is the filename that we want to create.
+            # Workbook() takes one, non-optional, argument which is the filename that we want to create.
             workbook = xlsxwriter.Workbook(filename + '.xlsx')
 
-            # The workbook object is then used to add new
-            # worksheet via the add_worksheet() method.
+            # The workbook object is then used to add new worksheet via the add_worksheet() method.
             worksheet = workbook.add_worksheet()
 
-            # Start from the first cell.
-            # Rows and columns are zero indexed.
+            # Start from the first cell. Rows and columns are zero indexed.
             row = 0
             column = 0
 
@@ -52,74 +49,56 @@ if __name__ == '__main__':
                     skip = False
 
                 # once you've found cell configuration proceed with the below
-                # Strips the newline character
-                if not line.startswith("//") and not line == "\n" and not line == " \n":
+                # Ignore new lines and comments
+                if re.match('^[A-Za-z1-9 ]*:.*;', line):
                     print("Line{}: {}".format(count, line))
 
                     split_data = line.replace(';', '').split(":")
                     print(split_data)
+                    # split data[0] = ADD CELL
+                    # split data[1] = LocalCellId=11, CellName="KYL1841", FreqBand=20, UlEarfcnCfgInd=NOT_CFG, DlEarfcn=6200, UlBandWidth=CELL_BW_N50, DlBandWidth=CELL_BW_N50, CellId=11, PhyCellId=15, FddTddInd=CELL_FDD, RootSequenceIdx=606,CELLRADIUS=38975, CustomizedBandWidthCfgInd=NOT_CFG, UePowerMaxCfgInd=NOT_CFG, MultiRruCellFlag=BOOLEAN_FALSE, TxRxMode=2T2R;
 
                     op_and_mo = split_data[0].split(" ")
-                    print(op_and_mo)
 
-                    print()
+                    operation = op_and_mo[0]
+                    manage_object = op_and_mo[1]
 
                     i = 0
                     print(split_data[1])
 
-                    parameters = split_data[1].split(',')
-                    len_param = len(parameters)
+                    parameters = split_data[1].replace(' ', '').split(',')
 
-                    flag = 0
+                    localcellid_found = False
                     if 'LOCALCELLID' in split_data[1] or 'LocalCellId' in split_data[1]:
-                        flag = 1
+                        localcellid_found = True
                         localcellid = parameters[0]
+                        localcellid_split = localcellid.split('=')
                         parameters.pop(0)
 
-                    if flag == 0:
-                        worksheet.write(row, column, op_and_mo[0])
-                        column += 1
-                        worksheet.write(row, column, op_and_mo[1])
-                        column += 1
-                        for param in parameters:
-                            other_param = param.split('=')
+                    for param in parameters:
+                        other_param = param.split('=')
+                        ampersand_param = other_param[1].replace('"', '').split('&')
+                        for ambersand in ampersand_param:
+                            worksheet.write(row, column, operation)
+                            column += 1
+                            worksheet.write(row, column, manage_object)
+                            column += 1
+
+                            if localcellid_found:
+                                worksheet.write(row, column, localcellid_split[0])
+                                column += 1
+                                worksheet.write(row, column, localcellid_split[1])
+                                column += 1
 
                             worksheet.write(row, column, other_param[0])
                             column += 1
 
-                            ampersand_param = other_param[1].split('&')
-                            for ambersand in ampersand_param:
-                                worksheet.write(row, column, ambersand)
-                                column += 1
-                    else:
-                        for param in parameters:
-                            worksheet.write(row, column, op_and_mo[0])
-                            column += 1
-                            worksheet.write(row, column, op_and_mo[1])
-                            column += 1
+                            worksheet.write(row, column, ambersand)
 
-                            localcellid_split = localcellid.split('=')
-                            worksheet.write(row, column, localcellid_split[0])
-                            column += 1
-                            worksheet.write(row, column, localcellid_split[1])
-                            column += 1
-
-                            other_param = param.split('=')
-
-                            worksheet.write(row, column, other_param[0])
-                            column += 1
-
-                            ampersand_param = other_param[1].split('&')
-                            for ambersand in ampersand_param:
-                                worksheet.write(row, column, ambersand)
-                                column += 1
-                            #
                             row += 1
                             column = 0
 
                     row += 1
                     column = 0
-                    # incrementing the value of row by one
-                    # with each iterations.
 
             workbook.close()
